23113050f92e68a490be8873a2446bb89b14d17b
alectramell <alectramell@gmail.com>
2017-06-19 16:47:57 -0700

    googledrive

--- render.sh
+++ render.sh
@@ -22,8 +22,8 @@ clear
 echo "[Desktop Entry]" > /home/$(whoami)/Desktop/google-drive.desktop
 echo "Name=Google Drive" >> /home/$(whoami)/Desktop/google-drive.desktop
 echo "Type=Application" >> /home/$(whoami)/Desktop/google-drive.desktop
-echo "Icon=/home//Desktop/google-drive/img/drive.svg" >> /home/$(whoami)/Desktop/google-drive.desktop
-echo "Exec=sesible-browser --new-tab='file:///home/$(whoami)/Desktop/google-drive/index.html'" >> /home/$(whoami)/Desktop/google-drive.desktop
+echo "Icon=/home/$(whoami)/Desktop/google-drive/img/drive.svg" >> /home/$(whoami)/Desktop/google-drive.desktop
+echo "Exec=sensible-browser --new-tab='file:///home/$(whoami)/Desktop/google-drive/index.html'" >> /home/$(whoami)/Desktop/google-drive.desktop
 echo "Terminal=false" >> /home/$(whoami)/Desktop/google-drive.desktop
 
 clear
