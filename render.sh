1a07118bf9f99b44e9a3f96856a42a4f2f3187f6
alectramell <alectramell@gmail.com>
2017-06-19 16:45:56 -0700

    googledrive

--- render.sh
+++ render.sh
@@ -15,4 +15,23 @@ zip -r google-drive.zip *.html *.json img js fonts
 
 clear
 
+touch /home/$(whoami)/Desktop/google-drive.desktop
+
+clear
+
+echo "[Desktop Entry]" > /home/$(whoami)/Desktop/google-drive.desktop
+echo "Name=Google Drive" >> /home/$(whoami)/Desktop/google-drive.desktop
+echo "Type=Application" >> /home/$(whoami)/Desktop/google-drive.desktop
+echo "Icon=/home//Desktop/google-drive/img/drive.svg" >> /home/$(whoami)/Desktop/google-drive.desktop
+echo "Exec=sesible-browser --new-tab='file:///home/$(whoami)/Desktop/google-drive/index.html'" >> /home/$(whoami)/Desktop/google-drive.desktop
+echo "Terminal=false" >> /home/$(whoami)/Desktop/google-drive.desktop
+
+clear
+
+chmod 755 /home/$(whoami)/Desktop/google-drive.desktop
+
+clear
+
 echo "DONE!" | pv -qL 10
+
+
